5564284a8055c59e5365460b047dd22ce1e861cd
PoHsi Kuo <pohsi.kuo@style.me>
2021-08-12 16:30:06 +0800

    Implement InfererenceApplication.

--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,7 @@
 #include "application.h"
 #include "common.h"
 #include "NvInfer.h"
+#include "buffers.h"
 #include <fstream>
 #include <unordered_map>
 #include <vector>
@@ -11,33 +12,68 @@ namespace {
 using namespace trtx;
 
 const char weightPath[] { "../data/model.wts" };
-const char enginePath[] { "./model.engine" };
+const char enginePath[] { "../data/model.engine" };
 const char inputBlobName[] { "data" };
 const char ouputBlobName[] { "prob" };
 constexpr size_t inputHeight{ 32 };
 constexpr size_t inputWidth{ 32 };
 constexpr int32_t outputSize{ 10 };
 
+using EnginePtr = std::shared_ptr<nvinfer1::ICudaEngine>;
+
+bool SaveEngine(const ICudaEngine& engine, const std::string_view &filePath, std::ostream &err) {
+    std::ofstream engineFile(filePath.data(), std::ios::binary);
+    if (!engineFile) {
+        err << "Cannot open engine file: " << filePath << std::endl;
+        return false;
+    }
+
+    auto serializedEngine{ infer_object(engine.serialize()) };
+    if (serializedEngine == nullptr) {
+        err << "Engine serialization failed" << std::endl;
+        return false;
+    }
+
+    engineFile.write(static_cast<char*>(serializedEngine->data()), serializedEngine->size());
+    return !engineFile.fail();
+}
+
+bool ReadReferenceFile(const std::string &fileName, std::vector<std::string> &refVector)
+{
+    std::ifstream infile(fileName);
+    if (!infile.is_open()) {
+        gLogError << "ERROR: readReferenceFile: Attempting to read from a file that is not open." << std::endl;
+        return false;
+    }
+    std::string line;
+    while (std::getline(infile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        refVector.push_back(line);
+    }
+    infile.close();
+    return true;
+}
+
 // Seralize to model
-class SerializeApplication : public Application {
+class APIToModelApplication : public Application {
     
 public:
-    explicit SerializeApplication(int batchSize)
+    explicit APIToModelApplication(int batchSize)
         : m_engine{ this->CreateEngine(batchSize) }
     {}
 
     virtual int Run() override {
 
         if (nullptr == this->m_engine) {
-            gLogError << "SerializeApplication::Run() error, engine is not create" << std::endl;
+            gLogError << "APIToModelApplication::Run() error, engine is not created" << std::endl;
             return -1;
         }
-        //TrtxUniquePtr<nvinfer1::IHostMemory> engine_plan{ trt_engine->serialize() };
-        //std::ofstream engine_file(engine_filename.c_str(), std::ios::binary);
-        //engine_file.write(reinterpret_cast<const char*>(engine_plan->data()), engine_plan->size());
-        //engine_file.close();
-
-        return 0;
+        auto enginePlan{ infer_object(this->m_engine->serialize()) };
+        bool result{ SaveEngine(*this->m_engine, enginePath, std::cerr) };
+        ASSERT(true == result);
+        return true == result ? 0 : -1;
     }
 
 private:
@@ -45,8 +81,6 @@ private:
     using WeightMap = std::unordered_map<std::string, nvinfer1::Weights>;
 
     using HostMemoryPtr = std::unique_ptr<HostMemory>;
-
-    using EnginePtr = std::shared_ptr<nvinfer1::ICudaEngine>;
 
     WeightMap LoadWeights(std::istream &is) {
 
@@ -68,7 +102,7 @@ private:
             // Load blob
             auto mem{ new TypedHostMemory<uint32_t, nvinfer1::DataType::kFLOAT>(size) };
             this->m_weightsMemory.emplace_back(mem);
-            auto val{ mem->Raw() };
+            auto val{ mem->raw() };
             for (uint32_t x = 0; x < size; ++x) {
                 is >> std::hex >> val[x];
             }
@@ -83,7 +117,7 @@ private:
 
 
     std::shared_ptr<nvinfer1::INetworkDefinition> BuildNetwork(nvinfer1::IBuilder &builder, const WeightMap &weightMap) {
-        auto network{ InferObject(builder.createNetworkV2(0)) };
+        auto network{ infer_object(builder.createNetworkV2(0)) };
         ASSERT(nullptr != network);
 
         auto tensor{ network->addInput(inputBlobName, nvinfer1::DataType::kFLOAT, nvinfer1::Dims{ 1, { inputHeight, inputWidth } }) };
@@ -155,14 +189,14 @@ private:
 
     EnginePtr CreateEngine(int batchSize) {
         gLogInfo << "Enter createEngine" << std::endl;
-        auto builder{ InferObject(nvinfer1::createInferBuilder(gLogger.getTRTLogger())) };
+        auto builder{ infer_object(nvinfer1::createInferBuilder(gLogger.getTRTLogger())) };
         if (nullptr == builder) {
             ASSERT(false && "CreateInferBuilder failed.");
             gLogError << "CreateInferBuilder failed" << std::endl;
             return nullptr;
         }
 
-        auto config{ InferObject(builder->createBuilderConfig()) };
+        auto config{ infer_object(builder->createBuilderConfig()) };
         if (nullptr == config) {
             ASSERT(false && "CreateBuilderConfig failed.");
             gLogError << "CreateBuilderConfig failed" << std::endl;
@@ -194,7 +228,7 @@ private:
         config->setMaxWorkspaceSize(1 << 20);
 
 
-        return  EnginePtr{ builder->buildEngineWithConfig(*network, *config), InferDeleter{} };
+        return EnginePtr{ builder->buildEngineWithConfig(*network, *config), InferDeleter{} };
     }
 
     std::vector<HostMemoryPtr> m_weightsMemory;
@@ -202,11 +236,139 @@ private:
     EnginePtr m_engine{ nullptr };
 };
 
-class DeserializeApplication : public Application {
+class InfererenceApplication : public Application {
 public:
 
+    explicit InfererenceApplication(int batchSize)
+        : m_batchSize{ batchSize }
+        , m_engine{ this->CreateEngine(batchSize) }
+    {}
+
     virtual int Run() override {
-       return 0;
+
+        if (nullptr == this->m_engine) {
+            gLogError << "InfererenceApplication::Run() error, engine is not created" << std::endl;
+            return -1;
+        }
+
+        if (false == GetInputOutputNames()) {
+            gLogError << "GetInputOutputNames failed" << std::endl;
+            return -1;
+        }
+
+        auto context{ infer_object(this->m_engine->createExecutionContext()) };
+        ASSERT(nullptr != context);
+        if (nullptr == context) {
+            gLogError << "CreateExecutionContext failed" << std::endl;
+            return -1;
+        }
+
+        BufferManager buffers{ this->m_engine, this->m_batchSize };
+
+        if (false == this->ProcessInput(buffers, inputHeight, inputWidth)) {
+            gLogError << "ProcessInput failed" << std::endl;
+            return -1;
+        }
+
+        cudaStream_t stream;
+        CHECK(cudaStreamCreate(&stream));
+
+
+        buffers.copyInputToDeviceAsync(stream);
+        if (!context->enqueueV2(buffers.getDeviceBindings().data(), stream, nullptr)) {
+            return -1;
+        }
+        buffers.copyOutputToHostAsync(stream);
+        cudaStreamSynchronize(stream);
+        cudaStreamDestroy(stream);
+        PrintOutput(buffers);
+        return 0;
+
+    }
+
+    EnginePtr CreateEngine(int) {
+
+        std::ifstream engineFile(enginePath, std::ios::binary);
+        if (engineFile.fail()) {
+            gLogError << "Load engine file failed, path: " << enginePath << std::endl;
+            return nullptr;
+        }
+
+        engineFile.seekg(0, std::ifstream::end);
+        auto fsize{ engineFile.tellg() };
+        engineFile.seekg(0, std::ifstream::beg);
+
+        std::vector<char> engineData(fsize);
+        engineFile.read(engineData.data(), fsize);
+
+        auto runtime{ infer_object(nvinfer1::createInferRuntime(gLogger.getTRTLogger())) };
+        EnginePtr engine{ runtime->deserializeCudaEngine(engineData.data(), fsize, nullptr) };
+        ASSERT(nullptr != engine && "DeserializeCudaEngine failed");
+        return engine;
+    }
+
+    bool GetInputOutputNames()
+    {
+        auto engine{ this->m_engine.get() };
+        const int numberOfBindings{ engine->getNbBindings() };
+        ASSERT(numberOfBindings == 2);
+        if (numberOfBindings != 2) {
+            return false;
+        }
+
+        for (int b = 0; b < numberOfBindings; ++b) {
+            const nvinfer1::Dims dims{ engine->getBindingDimensions(b) };
+            if (engine->bindingIsInput(b)) {
+                if (this->m_verbose) {
+                    gLogInfo << "Found input: " << engine->getBindingName(b) << " shape=" << dims
+                                    << " dtype=" << (int) engine->getBindingDataType(b) << std::endl;
+                }
+                this->m_inOut["input"] = engine->getBindingName(b);
+            }
+            else {
+                if (this->m_verbose) {
+                    gLogInfo << "Found output: " << engine->getBindingName(b) << " shape=" << dims
+                                    << " dtype=" << (int) engine->getBindingDataType(b) << std::endl;
+                }
+                this->m_inOut["output"] = engine->getBindingName(b);
+            }
+        }
+    }
+
+
+    bool ProcessInput(const trtx::BufferManager &buffers, int inputH, int inputW) {
+        //const std::vector<uint8_t> inputData(inputH * inputW, 1);
+        // Fill input buffer with all 1's which size is H * W
+        float *hostDataBuffer = static_cast<float*>(buffers.getHostBuffer(this->m_inOut["input"]));
+        for (int i = 0; i < inputH * inputW; i++) {
+            hostDataBuffer[i] = 1;
+        }
+        return true;
+    }
+
+    void PrintOutput(const BufferManager &buffers) const {
+        const float *probPtr{ static_cast<const float*>(buffers.getHostBuffer(this->m_inOut.at("output"))) };
+        std::cout << "Outout: ";
+        for (const auto &iter : std::vector<float>{ probPtr, probPtr + outputSize }) {
+            std::cout << iter << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    bool m_verbose{ true };
+
+    int m_batchSize{ 1 };
+
+    std::unordered_map<std::string, std::string> m_inOut;
+
+    EnginePtr m_engine{ nullptr };
+};
+
+class NullApplication : public Application {
+public:
+    virtual int Run() override {
+        std::cerr << "Option is required, try 'trtx --help' for more information" << std::endl;
+        return 0;
     }
 };
 
@@ -217,7 +379,13 @@ namespace trtx {
 Application::~Application() = default;
 
 ApplicationPtr ApplicationFactory::Create(const ApplicationBuildOption &option) const {
-    return ApplicationPtr{ new SerializeApplication{ option.m_batchSize } };
+    if (true == option.m_serailize) {
+        return ApplicationPtr{ new APIToModelApplication{ option.m_batchSize } };
+    }
+    else if (true == option.m_deserailize) {
+        return ApplicationPtr{ new InfererenceApplication{ option.m_batchSize } };
+    }
+    return ApplicationPtr{ new NullApplication{} };
 }
 
 }
